165386140c812e1f4295950c374c6a97ea312ba8
Daksha Patel <dakshapatel100@gmail.com>
2018-05-04 14:08:20 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,7 @@
-
-
 def my_each(array)
- if block_given?# put argument(s) here
-i = 0
-while i < array.length
-  yield (array[1])
-  i = i + 1
-
-  end
+  i = 0
+  while i < array.length
+    yield (array[1])
+  end 
   array
-
-
 end
